84c19efdfdfb3582bda19896bcd2341916bea28a
Mutalipov <sherhan.mutalip@mail.ru>
2026-01-31 01:40:14 +0500

    python variables/output

---
+++ w3school_examples/VARIABLES.output.py
@@ -0,0 +1,13 @@
+#Output variables
+x = "Python is awesome"
+print(x)
+#In the print() function, you output multiple variables, separated by a comma:
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+#using + in print()
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
