196456538fb9f858707f6b996ef6f472f3ee98e7
Mutalipov <sherhan.mutalip@mail.ru>
2026-01-31 01:25:20 +0500

    python comments

---
+++ w3school_examples/comments.py
@@ -0,0 +1,17 @@
+#creating comments
+#This is a comment
+print("Hello, World!")
+print("Hello, World!") #This is a comment
+#print("Hello, World!")
+print("Cheers, Mate!")
+#multiline comments
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
