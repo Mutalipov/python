0547bd553606c9c341214f1cb95ab5fdc5a16379
Mutalipov <sherhan.mutalip@mail.ru>
2026-01-31 01:23:12 +0500

    python output/numbers

---
+++ w3school_examples/output.numbers.py
@@ -0,0 +1,9 @@
+#printing numbers
+print(3)
+print(358)
+print(50000)
+#doing math operations in print() function
+print(3 + 3)
+print(2 * 5)
+#mix text and numbers in print() function
+print("I am", 35, "years old.")
