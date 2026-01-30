c2bbbff8fa80f53db078ec3eaf93211202847dae
Mutalipov <sherhan.mutalip@mail.ru>
2026-01-31 02:03:33 +0500

    python strings/slicing

---
+++ w3school_examples/STRINGS.slicing.py
@@ -0,0 +1,12 @@
+#slicing
+b = "Hello, World!"
+print(b[2:5])
+#slice from start
+b = "Hello, World!"
+print(b[:5])
+#slice to the end
+b = "Hello, World!"
+print(b[2:])
+#negative indexing
+b = "Hello, World!"
+print(b[-5:-2])
