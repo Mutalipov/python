ca6d3619a7e462f0c94f3de359a38266f0e19670
Mutalipov <sherhan.mutalip@mail.ru>
2026-01-31 01:30:07 +0500

    python variables/variables

---
+++ w3school_examples/VARIABLES.variables.py
@@ -0,0 +1,26 @@
+#creating variables
+x = 5
+y = "John"
+print(x)
+print(y)
+#variables don't need to be declared by type 
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+#casting
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+#get the type
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+#string variables can be declared by using single or double quotes
+x = "John"
+# is the same as
+x = 'John'
+#case sensitive
+a = 4
+A = "Sally"
+#A will not overwrite a
