62dba9b6c301427a43cac18d5605dc69bc781be0
Mutalipov <sherhan.mutalip@mail.ru>
2026-01-31 01:19:08 +0500

    python output/text

---
+++ w3school_examples/output.text.py
@@ -0,0 +1,9 @@
+#print text
+print("Hello World!")
+#printing many times
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+#using single or double quotes
+print("This will work!")
+print('This will also work!')
