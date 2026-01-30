7fe89b85ee3ddc7c4c5a3a94f0baa0db6a001894
Mutalipov <sherhan.mutalip@mail.ru>
2026-01-31 01:59:58 +0500

    python strings/strings

---
+++ w3school_examples/STRINGS.strings.py
@@ -0,0 +1,22 @@
+#'hello' is the same as "hello".
+print("Hello")
+print('Hello')
+#Quotes Inside Quotes
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+#Assign String to a Variable
+a = "Hello"
+print(a)
+#Multiline string
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+#or three single quote
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
