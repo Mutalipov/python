50917c06083f1c59fba8341f294f3ad1f4ca19c1
Mutalipov <sherhan.mutalip@mail.ru>
2026-01-31 01:36:03 +0500

    python variables/assigning values

---
+++ w3school_examples/VARIABLES.assigning_multi_values.py
@@ -0,0 +1,16 @@
+#Many values to many variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+#One value to many variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+#Unpack collection
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
