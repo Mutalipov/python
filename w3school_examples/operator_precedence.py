b85cfaf80c4db594d7c29c7c408423929f0bda9c
Mutalipov <sherhan.mutalip@mail.ru>
2026-01-31 11:02:49 +0500

    python operators/operator precedence

---
+++ w3school_examples/operator_precedence.py
@@ -0,0 +1,23 @@
+#Operator precedence
+print((6 + 3) - (6 + 3))
+print(100 + 5 * 3)
+
+#Precedence order
+'''
+()	Parentheses	
+**	Exponentiation	
++x  -x  ~x	Unary plus, unary minus, and bitwise NOT	
+*  /  //  %	Multiplication, division, floor division, and modulus	
++  -	Addition and subtraction	
+<<  >>	Bitwise left and right shifts	
+&	Bitwise AND	
+^	Bitwise XOR	
+|	Bitwise OR	
+==  !=  >  >=  <  <=  is  is not  in  not in 	Comparisons, identity, and membership operators	
+not	Logical NOT	
+and	AND	
+or	OR
+'''
+
+#Left-to-right evaluation
+print(5 + 4 - 7 + 3)
