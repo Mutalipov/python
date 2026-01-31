c462b1a8f075b5c473a7b230d955c0fec99c6474
Mutalipov <sherhan.mutalip@mail.ru>
2026-01-31 10:39:52 +0500

    python operators/assignment operators

---
+++ w3school_examples/OPERATORS.assignment_operators.py
@@ -0,0 +1,24 @@
+#Assignment operators
+'''
+=	x = 5	x = 5	
++=	x += 3	x = x + 3	
+-=	x -= 3	x = x - 3	
+*=	x *= 3	x = x * 3	
+/=	x /= 3	x = x / 3	
+%=	x %= 3	x = x % 3	
+//=	x //= 3	x = x // 3	
+**=	x **= 3	x = x ** 3	
+&=	x &= 3	x = x & 3	
+|=	x |= 3	x = x | 3	
+^=	x ^= 3	x = x ^ 3	
+>>=	x >>= 3	x = x >> 3	
+<<=	x <<= 3	x = x << 3	
+:=	print(x := 3)	x = 3
+print(x)
+'''
+
+#The Walrus Operator
+numbers = [1, 2, 3, 4, 5]
+
+if (count := len(numbers)) > 3:
+    print(f"List has {count} elements")
