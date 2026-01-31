95b60be0cafa138ba0210720d8a7d91881aa418c
Mutalipov <sherhan.mutalip@mail.ru>
2026-01-31 10:35:44 +0500

    python operators/arithmethic operators

---
+++ w3school_examples/OPERATORS.arithmethic_operators.py
@@ -0,0 +1,32 @@
+#Arithmethic Operators
+'''
++	Addition	x + y	
+-	Subtraction	x - y	
+*	Multiplication	x * y	
+/	Division	x / y	
+%	Modulus	x % y	
+**	Exponentiation	x ** y	
+//	Floor division	x // y
+'''
+#Example
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+#Division in Py
+x = 12
+y = 5
+
+print(x / y)
+
+x = 12
+y = 5
+
+print(x // y)
