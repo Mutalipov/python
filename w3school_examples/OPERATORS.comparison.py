6ae63887e867a93bd90620b2211dfb308c3500e8
Mutalipov <sherhan.mutalip@mail.ru>
2026-01-31 10:42:12 +0500

    python operators/comparison operators

---
+++ w3school_examples/OPERATORS.comparison.py
@@ -0,0 +1,27 @@
+#Comparison operators
+"""Operator	Name	Example	Try it
+==	Equal	x == y	
+!=	Not equal	x != y	
+>	Greater than	x > y	
+<	Less than	x < y	
+>=	Greater than or equal to	x >= y	
+<=	Less than or equal to	x <= y"""
+
+#Examples
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+#Chaining Comparison Operators
+x = 5
+
+print(1 < x < 10)
+
+print(1 < x and x < 10)
+ 
