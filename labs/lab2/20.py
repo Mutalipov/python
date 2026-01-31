9bff1140ecc5713f3b8a960e428788ee04b8eec6
Mutalipov <sherhan.mutalip@mail.ru>
2026-01-31 05:57:04 +0500

    20

---
+++ labs/lab2/20.py
@@ -0,0 +1,14 @@
+n = int(input())
+db = {}
+for _ in range(n):
+    y = input().split()
+    request = y[0]
+    key = y[1]
+    if request == "set":
+        value = y[2]
+        db[key] = value
+    elif request == "get":
+        if(key in db.keys()):
+            print(db[key])
+        else:
+            print(f"KE: no key {key} found in the document")
