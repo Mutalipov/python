40cf6b0f3ed682e13c25226d77e68f6fdaff80cc
Mutalipov <sherhan.mutalip@mail.ru>
2026-02-17 21:16:22 +0500

    return values

---
+++ w3school_examples/prac3/functions/return_values.py
@@ -0,0 +1,15 @@
+def get_greeting():
+  return "Hello from a function"
+
+message = get_greeting()
+print(message)
+
+def get_greeting():
+  return "Hello from a function"
+
+print(get_greeting())
+
+#pass statement
+def my_function():
+  pass
+
